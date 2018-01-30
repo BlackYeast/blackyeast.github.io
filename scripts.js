792d4abc4b283514345bbe979c5579cae45674fe
Chris <christopher.ravailhe@pucpr.edu.br>
2018-01-30 02:28:40 -0200

    Included particles and shake animation.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ $(function () {
 });
 
 $(document).ready(function () {
+
     var movementStrength = 25;
     var height = movementStrength / $(window).height();
     var width = movementStrength / $(window).width();
@@ -18,5 +19,33 @@ $(document).ready(function () {
         var newvalueY = height * pageY * -1 - 50;
         $('#div_bg_movable').css("background-position", newvalueX + "px     " + newvalueY + "px");
     });
+
+    // bg img flicking animation
+    $("#div_byName").shake();
 });
 
+var shakeDelay;
+var shakeY;
+var shakeTime;
+
+jQuery.fn.shake = function () {
+    this.each(function (i) {
+        $(this).css({ "position": "relative" });
+        for (var x = 1; x <= 100; x++) {
+            shakeDelay = Math.random() * 10000;
+            shakeY = Math.random() * 10;
+            shakeTime = Math.random() * 20;
+            $(this).fadeTo(shakeTime, Math.random() + 0.5);
+            $(this).animate({ top: -shakeY }, shakeTime).animate({ top: shakeY }, shakeTime).animate({ top: 0 }, shakeTime);
+            $(this).fadeTo(shakeTime, 1);
+            $(this).delay(shakeDelay);
+
+        }
+    });
+    return this;
+}
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particlesjs-config.json', function () {
+    console.log('callback - particles.js config loaded');
+});

308a7a81ea315ac9bb54217090a5c636f51fdb25
Chris <christopher.ravailhe@pucpr.edu.br>
2018-01-30 01:15:08 -0200

    Included loader to slow load imgs.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
+$(function () {
+    $(".img_highres").off().on("load", function () {
+        var id = $(this).attr("id");
+        var highres = $(this).attr("src").toString();
+        var target = "#div_" + id.substring(4);
+        $(target).css("background-image", "url(" + highres + ")");
+    });
+});
+
 $(document).ready(function () {
     var movementStrength = 25;
     var height = movementStrength / $(window).height();
     var width = movementStrength / $(window).width();
-    $("#bg_movable").mousemove(function (e) {
+    $("#div_bg_movable").mousemove(function (e) {
         var pageX = e.pageX - ($(window).width() / 2);
         var pageY = e.pageY - ($(window).height() / 2);
         var newvalueX = width * pageX * -1 - 25;
         var newvalueY = height * pageY * -1 - 50;
-        $('#bg_movable').css("background-position", newvalueX + "px     " + newvalueY + "px");
+        $('#div_bg_movable').css("background-position", newvalueX + "px     " + newvalueY + "px");
     });
 });
 
